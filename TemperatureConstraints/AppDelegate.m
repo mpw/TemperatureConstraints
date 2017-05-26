d2a4553b4e09e48f53011855d2e75659917766a0
Marcel Weiher <marcel@metaobject.com>
2017-05-26 16:56:09 +0200

    switch from plist to .st format for Smalltalk code

--- TemperatureConstraints/AppDelegate.m
+++ TemperatureConstraints/AppDelegate.m
@@ -28,18 +28,39 @@ intAccessor( c, setC)
 intAccessor( k, setK)
 intAccessor( f, setF)
 
--interpreter
+-(MPWStCompiler*)interpreter
 {
     return [[self methodServer] interpreter];
 }
 
 
 - (void)applicationWillFinishLaunching:(NSNotification *)aNotification {
-    [self setMethodServer:[[[MethodServer alloc] initWithMethodDictName:@"temperatureconstraints"] autorelease]];
+    [self setMethodServer:[[[MethodServer alloc] initWithMethodDictName:@"temperatureconverter"] autorelease]];
     [[self methodServer] setupMethodServer];
-    NSLog(@"interpreter: %@ solver: %@",[self interpreter],[[self interpreter] solver]);
+    NSURL *stURL=[[NSBundle mainBundle] URLForResource:@"AppDelegate" withExtension:@"st"];
+    NSLog(@"url: %@",stURL);
+    NSString *st=[NSString stringWithContentsOfURL:stURL encoding:NSUTF8StringEncoding error:nil];
+    NSLog(@"script: %@",st);
     [[self interpreter] bindValue:self toVariableNamed:@"delegate"];
     [[self interpreter] evaluateScriptString:@"scheme:ivar := ref:var:delegate asScheme."];
+    [self setC:0];
+    [self setF:32];
+    [self setK:273];
+
+    NSLog(@"compiler: %@",[self interpreter]);
+    @try {
+        [[self interpreter] evaluateScriptString:st];
+    } @catch ( NSException *e) {
+        NSLog(@"error evaluating: %@",e);
+    }
+    NSLog(@"interpreter: %@ solver: %@",[self interpreter],[[self interpreter] solver]);
+
+    
+    
+    [[self interpreter] evaluateScriptString:@"stdout println: ivar:c."];
+    [[self interpreter] evaluateScriptString:@"stdout println: ivar:f."];
+    [[self interpreter] evaluateScriptString:@"stdout println: ivar:k."];
+    
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(methodsDefined) name:@"methodsDefined" object:nil];
 }
@@ -58,11 +79,18 @@ intAccessor( f, setF)
 
 -(void)methodsDefined
 {
+    NSLog(@"=== methods defined, re-init");
     [self setSolver:[NSClassFromString(@"DBSolver") solver]];
     [[self interpreter] setSolver:[self solver]];
     [self setupDeltablueConstraints];
 }
 
+-(void)dump:(NSString*)message
+{
+    NSLog(@"=== %@ ===",message);
+    NSLog(@"c=%d f=%d k=%d",c,f,k);
+    NSLog(@"======");
+}
 
 @end
 
@@ -71,7 +99,11 @@ intAccessor( f, setF)
 -(void)objst_addObserver:anObserver forKey:aKey
 {
     [NSNotificationCenter.defaultCenter addObserverForName:NSControlTextDidChangeNotification object:self queue:nil usingBlock:^(NSNotification *note) {
+        [[[NSApplication sharedApplication] delegate] dump:@"before"];
+        NSLog(@"self value: %@ obserer: %@",[self objectValue],anObserver);
+        NSLog(@"self value: %@ obserer: %@",[self objectValue],anObserver);
         [anObserver didChange];
+        [[[NSApplication sharedApplication] delegate] dump:@"after"];
     }];
 }
 
